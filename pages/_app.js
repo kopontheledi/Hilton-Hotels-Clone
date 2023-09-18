0b3d5de5aafa83b96033dc56f53fdd15c8372a61
Kopo Ntheledi <kopontheledi@gmail.com>
2023-09-18 10:27:15 +0200

    added more files

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import { Footer } from './components/footer'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return ( 
+  <div>
+    <Component {...pageProps} />
+    <Footer />
+  </div>
+  )
 }
 
 export default MyApp
